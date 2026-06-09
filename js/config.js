1399bd046c5e70c0a9edf4b220f9fa9c564f1f70
RENEGADE21AI <184287164+RENEGADE21AI@users.noreply.github.com>
2026-06-08 21:06:11 -0400

    Remove obsolete loader config module

--- js/config.js
+++
@@ -1,3 +0,0 @@
-export const LEGACY_BUILD_URL = "https://raw.githubusercontent.com/RENEGADE21AI/Star_Strike_RUSH/a5cf58cf7acb72db2dc147261d3dc28f094794b7/index.html";
-
-export const LOAD_ERROR_MESSAGE = "Star Strike RUSH failed to load. Check the console for details.";
